9931ad280cdefbe321845e9c4e68e7b75dc694bf
Dutchman007 <tosinkazeem77@gmail.com>
2025-09-01 23:05:09 +0100

    first commit

--- attendance.py
+++ attendance.py
@@ -28,65 +28,61 @@ def register_student(student_id, name):
                 "absent_days" : []
 		}
 		print(attendance)
+		return attendance
 	else:
 		print(f"Student ID {student_id} already exists.")
-register_student(1, "Tosin")
+register_student("S1", "Tosin")
+register_student("S2", "Bob")
+register_student("S3", "Alice")
     #pass
 
-def mark_present(*student_ids):
-    #"""Mark multiple students as present for today."""
+def mark_present(student_ids):
 	today = str(datetime.date.today())
-	for sid in student_ids:
-		if sid in attendance:
-			if today not in attendance[sid]["present_days"]:
-                		attendance[sid]["present_days"].append(today)
-            
-	    		if today in attendance[sid]["absent_days"]:
-                		attendance[sid]["absent_days"].remove(today)
-        	else:
-            		print(f"Student ID {sid} not found.")
-mark_present(1, 3)
-   # implement logic
-    #pass
+	for id_ in student_ids:
+		if id_ in attendance:
+			if today not in attendance[id_]["present_days"]:
+				attendance[id_]["present_days"].append(today)
+			
+			if today in attendance[id_]["absent_days"]:
+				attendance[id_]["absent_days"].remove(today)
+			print(attendance[id_])
+		else:
+			print(f"Student ID {id_} not found!")
+mark_present(["S1", "S2"])
 
-def mark_absent(*student_ids):
-    #"""Mark multiple students as absent for today."""
+def mark_absent(student_ids):
 	today = str(datetime.date.today())
-    	for sid in student_ids:
-        	if sid in attendance:
-            		if today not in attendance[sid]["absent_days"]:
-                		attendance[sid]["absent_days"].append(today)
-            		
-			if today in attendance[sid]["present_days"]:
-                		attendance[sid]["present_days"].remove(today)
-        	else:
-            		print(f"Student ID {sid} not found.")
-mark_absent(1, 2, 3)
-    #implement logic
-    #pass
+	for id_ in student_ids:
+		if id_ in attendance:
+			if today not in attendance[id_]["absent_days"]:
+				attendance[id_]["absent_days"].append(today)
+			if today in attendance[id_]["present_days"]:
+				attendance[id_]["present_days"].remove(today)
+			print(attendance[id_])
+		else:
+			print(f"Student ID {id_} not found!")
+mark_absent(["S3"])
 
 def get_report(**kwargs):
-    #"""Generate attendance report with optional filters."""
 	report = {}
 	today = str(datetime.date.today())
+	for id_, data in attendance.items():
+		inside = True
 
-    	for sid, data in attendance.items():
-        	only_present = True
+		if kwargs.get("student_id") and kwargs["student_id"] != id_:
+			inside = False
 
-        	if kwargs.get("student_id") and kwargs["student_id"] != sid:
-            		only_present = False
+		if kwargs.get("only_present"):
+			inside = today in data["present_days"]
 
-        	if kwargs.get("only_present"):
-            		only_present = today in data["present_days"]
+		if kwargs.get("only_absent"):
+			inside = today in data["absent_days"]
 
-        	if kwargs.get("only_absent"):
-            		only_present = today in data["absent_days"]
-
-        	if include:
-            		report[sid] = data
-
-    	return report
-get_report(only_present = True)
-
-    # implement logic
+		if inside:
+			report[id_] = data
+	return report
+print(get_report())
+print(get_report(only_present = True))
+print(get_report(only_absent = True))
+print(get_report(student_id = "S1"))
 
